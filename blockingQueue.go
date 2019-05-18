0ea4e3414b5ed0837281096db5d132ff6162047c
pavel.varyukhichev <dae.fromru@gmail.com>
2019-05-18 22:08:53 +0300

    add limited blocking queue

--- blockingQueue.go
+++ blockingQueue.go
@@ -5,7 +5,15 @@ import (
 	"sync/atomic"
 )
 
-type BlockingQueue struct {
+type Queue interface {
+	Put(data interface{}) bool
+	Get(nonblocking ...bool) (interface{}, bool)
+	Drain()
+	Reset()
+	Len() int
+}
+
+type blockingQueue struct {
 	queue   []interface{}
 	len     int32
 	lock    sync.Mutex
@@ -13,8 +21,8 @@ type BlockingQueue struct {
 	drained bool
 }
 
-func NewBlockingQueue() *BlockingQueue {
-	bq := &BlockingQueue{
+func NewBlockingQueue() Queue {
+	bq := &blockingQueue{
 		queue: []interface{}{},
 	}
 	bq.cond = sync.NewCond(&bq.lock)
@@ -22,8 +30,9 @@ func NewBlockingQueue() *BlockingQueue {
 	return bq
 }
 
-// Adds data to queue and awakes one of waiting for data goroutines if there are any, returns <true> if data was put to queue.
-func (bq *BlockingQueue) Put(data interface{}) bool {
+// Adds data to queue and awakes one of waiting for data goroutines if there are any, returns <true> if data was put to
+// queue.
+func (bq *blockingQueue) Put(data interface{}) bool {
 	bq.lock.Lock()
 	if bq.drained {
 		bq.lock.Unlock()
@@ -39,7 +48,7 @@ func (bq *BlockingQueue) Put(data interface{}) bool {
 	return true
 }
 
-func (bq *BlockingQueue) internalGetWithUnlock() (interface{}, bool) {
+func (bq *blockingQueue) internalGetWithUnlock() (interface{}, bool) {
 	if bq.drained {
 		bq.lock.Unlock()
 
@@ -60,7 +69,7 @@ func (bq *BlockingQueue) internalGetWithUnlock() (interface{}, bool) {
 
 // Get() will read next value from queue or block until there is data. Also returns "drained" sign.
 // Given nonblocking <true> as a parameter, returns immediately with the value on top of queue or nil if it is empty.
-func (bq *BlockingQueue) Get(nonblocking ...bool) (interface{}, bool) {
+func (bq *blockingQueue) Get(nonblocking ...bool) (interface{}, bool) {
 	bq.lock.Lock()
 
 	for {
@@ -81,7 +90,7 @@ func (bq *BlockingQueue) Get(nonblocking ...bool) (interface{}, bool) {
 
 // Clears queue and releases all blocking goroutines causing them to get <nil> as data.
 // Drained queue will not receive any data.
-func (bq *BlockingQueue) Drain() {
+func (bq *blockingQueue) Drain() {
 	bq.lock.Lock()
 	bq.drained = true
 	bq.queue = bq.queue[:0]
@@ -91,13 +100,88 @@ func (bq *BlockingQueue) Drain() {
 }
 
 // Activates queue to start receiving data again.
-func (bq *BlockingQueue) Reset() {
+func (bq *blockingQueue) Reset() {
 	bq.lock.Lock()
 	bq.drained = false
 	bq.lock.Unlock()
 }
 
 // Queue size.
-func (bq *BlockingQueue) Len() int {
+func (bq *blockingQueue) Len() int {
 	return int(atomic.LoadInt32(&bq.len))
+}
+
+type limitedBlockingQueue struct {
+	queue   chan interface{}
+	limit   int
+	len     int32
+	drained bool
+	lock    sync.Mutex
+}
+
+func NewLimitedBlockingQueue(limit int) Queue {
+	return &limitedBlockingQueue{
+		queue:   make(chan interface{}, limit),
+		drained: false,
+		limit:   limit,
+	}
+}
+
+// Adds data to queue and awakes one of waiting for data goroutines if there are any, returns <true> if data was put to
+// queue. Blocks if queue is full.
+func (lbq *limitedBlockingQueue) Put(data interface{}) bool {
+	ret := true
+	defer func() {
+		// recover in case of send to closed channel
+		if recover() != nil {
+			ret = false
+		}
+	}()
+	lbq.queue <- data
+	atomic.AddInt32(&lbq.len, 1)
+
+	return ret
+}
+
+func (lbq *limitedBlockingQueue) Get(nonblocking ...bool) (interface{}, bool) {
+	lbq.lock.Lock()
+	defer lbq.lock.Unlock()
+
+	if lbq.drained {
+		return nil, true
+	}
+
+	if len(nonblocking) > 0 && nonblocking[0] {
+		select {
+		case data := <-lbq.queue:
+			atomic.AddInt32(&lbq.len, -1)
+			return data, false
+		default:
+			return nil, false
+		}
+	}
+	data := <-lbq.queue
+	atomic.AddInt32(&lbq.len, -1)
+
+	return data, false
+}
+
+func (lbq *limitedBlockingQueue) Drain() {
+	close(lbq.queue)
+	lbq.lock.Lock()
+	lbq.drained = true
+	atomic.StoreInt32(&lbq.len, 0)
+	lbq.lock.Unlock()
+}
+
+func (lbq *limitedBlockingQueue) Reset() {
+	lbq.lock.Lock()
+	lbq.queue = make(chan interface{}, lbq.limit)
+	lbq.drained = false
+	atomic.StoreInt32(&lbq.len, 0)
+	lbq.lock.Unlock()
+}
+
+func (lbq *limitedBlockingQueue) Len() int {
+	return int(atomic.LoadInt32(&lbq.len))
 }
--- blockingQueue_test.go
+++ blockingQueue_test.go
@@ -35,3 +35,47 @@ func TestBlockingQueue(t *testing.T) {
 		t.Fatal("Not all values passed through queue")
 	}
 }
+
+func TestLimitedBlockingQueue(t *testing.T) {
+	queue := NewLimitedBlockingQueue(100)
+	var cnt int32
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for {
+				data, drained := queue.Get()
+				if drained {
+					break
+				}
+				if _, ok := data.(int); ok {
+					atomic.AddInt32(&cnt, 1)
+				}
+			}
+		}()
+	}
+
+	values := int(1e6)
+
+	for i := 0; i < values; i++ {
+		queue.Put(i)
+	}
+
+	time.Sleep(time.Second * 10)
+	if cnt != int32(values) {
+		t.Fatal("Not all values passed through queue")
+	}
+}
+
+func TestPutToDrainedQueue(t *testing.T) {
+	bq := NewBlockingQueue()
+	bq.Drain()
+	if bq.Put(1) {
+		t.Fatal("Drained blocking queue Put was successful")
+	}
+
+	lbq := NewLimitedBlockingQueue(5)
+	lbq.Drain()
+	if lbq.Put(1) {
+		t.Fatal("Drained limited blocking queue Put was successful")
+	}
+}
